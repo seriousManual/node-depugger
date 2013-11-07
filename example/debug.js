04486a6b247fc1d5f259e038bf4c4e7adc5e56e0
zaphod1984 <zaphod84@gmx.de>
2013-11-07 14:18:55 +0100

    added examples

--- example/debug.js
+++ example/debug.js
@@ -9,3 +9,7 @@ debug('spam %d eggs', 10);
 depugger(true, 'fooDebugger')('foo');
 
 depugger(true)('foo');
+
+depugger(false)('hello, i will not be logged!');
+
+depugger(false, 'barDebugger')('hello, i will not be logged, too!');
