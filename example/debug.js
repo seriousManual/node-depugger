af9650146bf81477d46975ee61ef0cdc90a24b95
zaphod1984 <zaphod84@gmx.de>
2013-11-07 14:17:30 +0100

    added examples

--- example/debug.js
+++ example/debug.js
@@ -5,3 +5,7 @@ var debug = depugger({debug: true, name: 'fooDebugger'});
 debug('foo');
 debug('bar "%s"', 'bax');
 debug('spam %d eggs', 10);
+
+depugger(true, 'fooDebugger')('foo');
+
+depugger(true)('foo');
