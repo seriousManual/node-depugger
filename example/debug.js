6384a95276e7618ea7ed87aeea529da59f409b30
zaphod1984 <zaphod84@gmx.de>
2013-11-07 14:23:08 +0100

    better example

--- example/debug.js
+++ example/debug.js
@@ -2,14 +2,14 @@ var depugger = require('../');
 
 var debug = depugger({debug: true, name: 'fooDebugger'});
 
-debug('foo');
-debug('bar "%s"', 'bax');
-debug('spam %d eggs', 10);
+debug('foo'); //output: [fooDebugger] foo
+debug('bar "%s"', 'bax'); //output: [fooDebugger] bar "bax"
+debug('spam %d eggs', 10); //output: [fooDebugger] spam 10 eggs
 
-depugger(true, 'fooDebugger')('foo');
+depugger(true, 'fooDebugger')('foo'); //output: [fooDebugger] foo
 
-depugger(true)('foo');
+depugger(true)('foo'); //output: foo
 
-depugger(false)('hello, i will not be logged!');
+depugger(false)('hello, i will not be logged!'); //output: -
 
-depugger(false, 'barDebugger')('hello, i will not be logged, too!');
+depugger(false, 'barDebugger')('hello, i will not be logged, too!'); //output: -
