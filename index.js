e131cafca7c54ac5181092b2f557973926c571eb
zaphod1984 <zaphod84@gmx.de>
2013-11-26 17:38:12 +0100

    refactored

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ var noop = function() {};
  * the logging of the messages happens in the context of the initializing factory method
  * @param pDebug flag that indicates if debugging should be activated
  * @param pName context name for debugging messages
- * @param pOptions options hash
  * @return {Function}
  */
 
-function depugger(pDebug, pName, pOptions) {
-    var debug, name, options, backend;
+function depugger(pDebug, pName) {
+    var debug, name, backend;
 
     if (typeof pDebug === 'object') {
         debug = !!pDebug.debug;
@@ -28,21 +27,17 @@ function depugger(pDebug, pName, pOptions) {
 
     var prefix = name ? util.format('[%s] ', name) : '';
 
-    var logFunction = noop;
+    var logFunction = function() {
+        var args = Array.prototype.splice.call(arguments, 0);
+        var message = util.format.apply(null, args);
 
-    if(debug) {
-        logFunction = function() {
-            if (!debug) {
-                return;
-            }
+        message = prefix + message;
 
-            var args = Array.prototype.splice.call(arguments, 0);
-            var message = util.format.apply(null, args);
+        backend.write(message);
+    };
 
-            message = prefix + message;
-
-            backend.write(message);
-        };
+    if(!debug) {
+        logFunction = noop;
     }
 
     logFunction.child = function(childName) {
